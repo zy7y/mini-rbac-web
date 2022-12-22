23b76fffa8f7341dd7900d9c0412a4d03119b841
zy7y <13271962515@163.com>
2022-12-22 23:52:12 +0800

    feat: 登录页面&组件库引入

---
+++ src/views/login/LoginView.vue
@@ -0,0 +1,93 @@
+<script setup lang="ts">
+import { UserOutlined, LockOutlined } from '@ant-design/icons-vue'
+import { reactive } from 'vue'
+import type { UnwrapRef } from 'vue'
+import type { FormProps } from 'ant-design-vue'
+
+interface FormState {
+  username: string
+  password: string
+}
+const formState: UnwrapRef<FormState> = reactive({
+  username: 'admin',
+  password: '123456'
+})
+const handleFinish: FormProps['onFinish'] = (values) => {
+  console.log(values, formState)
+}
+const handleFinishFailed: FormProps['onFinishFailed'] = (errors) => {
+  console.log(errors)
+}
+</script>
+
+<template>
+  <div class="login">
+    <div class="login-continer">
+      <h2>Mini RBAC</h2>
+      <a-form
+        :model="formState"
+        @finish="handleFinish"
+        @finishFailed="handleFinishFailed"
+      >
+        <a-form-item>
+          <a-input v-model:value="formState.username" placeholder="账号">
+            <template #prefix
+              ><UserOutlined style="color: rgba(0, 0, 0, 0.25)"
+            /></template>
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-input
+            v-model:value="formState.password"
+            type="password"
+            placeholder="密码"
+          >
+            <template #prefix
+              ><LockOutlined style="color: rgba(0, 0, 0, 0.25)"
+            /></template>
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-button
+            type="primary"
+            style="width: 100%"
+            html-type="submit"
+            :disabled="formState.username === '' || formState.password === ''"
+          >
+            登录
+          </a-button>
+        </a-form-item>
+      </a-form>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.login {
+  display: flex;
+  width: 100wh;
+  height: 100vh;
+  /* 主轴居中 */
+  justify-content: center;
+  /* 交叉轴居中,需要有宽高 */
+  align-items: center;
+
+  background: url('http://49.232.203.244:1856/assets/background.d7103c44.svg');
+}
+
+.login-continer {
+  /* 文本居中 */
+  text-align: center;
+  width: 300px;
+  height: 300px;
+  padding: 20px;
+}
+.login-continer:hover {
+  border: 1px solid rgba(220, 220, 220, 0.1);
+  border-radius: 2rem;
+}
+/* 最后一个item 元素 外边距 */
+.ant-form-item:last-child {
+  margin-bottom: 0px !important;
+}
+</style>
